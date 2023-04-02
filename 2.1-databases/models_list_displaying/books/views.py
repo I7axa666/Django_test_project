f334fbd41455669b2e8d3a7792e0b5b87267ad2b
Paul Solovey <p.v.solovey@ya.ru>
2023-04-02 21:07:05 +0300

    HomeWork Online-library is done

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,41 @@
+from datetime import datetime
+
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
+from books.models import Book
 
-def books_view(request):
-    template = 'books/books_list.html'
-    context = {}
+CONTENT = Book.objects.order_by('pub_date')
+
+def books_view(request, pub_date=None):
+
+    if pub_date is None:
+        template = 'books/books_list.html'
+
+        context = {
+            'books': CONTENT,
+
+        }
+    else:
+        template = 'books/pub_date_book.html'
+        page_number = int(request.GET.get('page', 1))
+        pagi = Paginator(CONTENT, 1)
+        for number in range(1, pagi.count + 1):
+            page = pagi.get_page(number)
+            if pub_date == str(page.object_list[0].pub_date):
+                page_number = number
+
+        page = pagi.get_page(page_number)
+        if page.has_previous:
+            prev_page = page_number - 1
+
+        if page.has_next:
+            next_page = page_number + 1
+        context = {
+            'page': page,
+            'next': str(pagi.get_page(next_page).object_list[0].pub_date),
+            'prev': str(pagi.get_page(prev_page).object_list[0].pub_date),
+
+        }
+
     return render(request, template, context)
